2c6985bd991ce539e0e8b2ac47ab07dd33956569
rain <yunwei.zh@gmail.com>
2021-09-26 00:22:46 +0800

    修改默认卡组

--- scripts/vscripts/config/new_comer_cardgroup.lua
+++ scripts/vscripts/config/new_comer_cardgroup.lua
@@ -1,266 +1,88 @@
 NEW_COMER_CARDGROUP = {
-    ["npc_dota_hero_legion_commander"] =
-    {
-        {
-            name = "ability_custom_lion_impale",
-            count = 1
-        },
-        {
-            name = "ability_custom_voodoo",
-            count = 1
-        },
-        {
-            name = "ability_custom_breathe_fire",
-            count = 1
-        },
-        {
-            name = "ability_custom_split_earth",
-            count = 1
-        },
-        {
-            name = "ability_custom_root_attack",
-            count = 1
-        },
+    ["npc_dota_hero_lina"] = {
         {
             name = "ability_custom_lihuo",
             count = 1
         },
         {
-            name = "ability_custom_refraction",
+            name = "ability_custom_xiuxiu",
             count = 1
         },
         {
-            name = "ability_custom_spiked_carapace",
+            name = "ability_custom_spider",
             count = 1
         },
         {
-            name = "ability_custom_call_summon_omniknight",
+            name = "ability_custom_ghost_book",
             count = 1
         },
         {
-            name = "ability_custom_call_summon_magnataur",
+            name = "ability_custom_cloud",
+            count = 1
+        },
+        {
+            name = "ability_custom_arc_lightning",
+            count = 1
+        },
+        {
+            name = "ability_custom_cat",
+            count = 1
+        },
+        {
+            name = "ability_custom_acid_spray",
+            count = 1
+        },
+        {
+            name = "ability_custom_sniper",
+            count = 1
+        },
+        {
+            name = "ability_custom_voodoo",
             count = 1
         },
     },
 
-    ["npc_dota_hero_spectre"] =
-    {
+    ["npc_dota_hero_templar_assassin"] = {
         {
-            name = "ability_custom_lion_impale",
+            name = "ability_custom_lihuo",
+            count = 1
+        },
+        {
+            name = "ability_custom_xiuxiu",
+            count = 1
+        },
+        {
+            name = "ability_custom_spider",
+            count = 1
+        },
+        {
+            name = "ability_custom_ghost_book",
+            count = 1
+        },
+        {
+            name = "ability_custom_cloud",
+            count = 1
+        },
+        {
+            name = "ability_custom_arc_lightning",
+            count = 1
+        },
+        {
+            name = "ability_custom_cat",
+            count = 1
+        },
+        {
+            name = "ability_custom_acid_spray",
+            count = 1
+        },
+        {
+            name = "ability_custom_sniper",
             count = 1
         },
         {
             name = "ability_custom_voodoo",
             count = 1
         },
-        {
-            name = "ability_custom_breathe_fire",
-            count = 1
-        },
-        {
-            name = "ability_custom_split_earth",
-            count = 1
-        },
-        {
-            name = "ability_custom_root_attack",
-            count = 1
-        },
-        {
-            name = "ability_custom_lihuo",
-            count = 1
-        },
-        {
-            name = "ability_custom_refraction",
-            count = 1
-        },
-        {
-            name = "ability_custom_spiked_carapace",
-            count = 1
-        },
-        {
-            name = "ability_custom_call_summon_omniknight",
-            count = 1
-        },
-        {
-            name = "ability_custom_call_summon_magnataur",
-            count = 1
-        },
-    },
-
-    ["npc_dota_hero_slark"] =
-    {
-        {
-            name = "ability_custom_lion_impale",
-            count = 1
-        },
-        {
-            name = "ability_custom_voodoo",
-            count = 1
-        },
-        {
-            name = "ability_custom_breathe_fire",
-            count = 1
-        },
-        {
-            name = "ability_custom_split_earth",
-            count = 1
-        },
-        {
-            name = "ability_custom_root_attack",
-            count = 1
-        },
-        {
-            name = "ability_custom_lihuo",
-            count = 1
-        },
-        {
-            name = "ability_custom_refraction",
-            count = 1
-        },
-        {
-            name = "ability_custom_spiked_carapace",
-            count = 1
-        },
-        {
-            name = "ability_custom_call_summon_omniknight",
-            count = 1
-        },
-        {
-            name = "ability_custom_call_summon_magnataur",
-            count = 1
-        },
-    },
-
-    ["npc_dota_hero_juggernaut"] =
-    {
-        {
-            name = "ability_custom_lion_impale",
-            count = 1
-        },
-        {
-            name = "ability_custom_voodoo",
-            count = 1
-        },
-        {
-            name = "ability_custom_breathe_fire",
-            count = 1
-        },
-        {
-            name = "ability_custom_split_earth",
-            count = 1
-        },
-        {
-            name = "ability_custom_root_attack",
-            count = 1
-        },
-        {
-            name = "ability_custom_lihuo",
-            count = 1
-        },
-        {
-            name = "ability_custom_refraction",
-            count = 1
-        },
-        {
-            name = "ability_custom_spiked_carapace",
-            count = 1
-        },
-        {
-            name = "ability_custom_call_summon_omniknight",
-            count = 1
-        },
-        {
-            name = "ability_custom_call_summon_magnataur",
-            count = 1
-        },
-    },
-
-    ["npc_dota_hero_lina"] =
-    {
-        {
-            name = "ability_custom_lion_impale",
-            count = 1
-        },
-        {
-            name = "ability_custom_voodoo",
-            count = 1
-        },
-        {
-            name = "ability_custom_breathe_fire",
-            count = 1
-        },
-        {
-            name = "ability_custom_split_earth",
-            count = 1
-        },
-        {
-            name = "ability_custom_root_attack",
-            count = 1
-        },
-        {
-            name = "ability_custom_lihuo",
-            count = 1
-        },
-        {
-            name = "ability_custom_refraction",
-            count = 1
-        },
-        {
-            name = "ability_custom_spiked_carapace",
-            count = 1
-        },
-        {
-            name = "ability_custom_call_summon_omniknight",
-            count = 1
-        },
-        {
-            name = "ability_custom_call_summon_magnataur",
-            count = 1
-        },
-    },
-
-    ["npc_dota_hero_templar_assassin"] =
-    {
-        {
-            name = "ability_custom_lion_impale",
-            count = 1
-        },
-        {
-            name = "ability_custom_voodoo",
-            count = 1
-        },
-        {
-            name = "ability_custom_breathe_fire",
-            count = 1
-        },
-        {
-            name = "ability_custom_split_earth",
-            count = 1
-        },
-        {
-            name = "ability_custom_root_attack",
-            count = 1
-        },
-        {
-            name = "ability_custom_lihuo",
-            count = 1
-        },
-        {
-            name = "ability_custom_refraction",
-            count = 1
-        },
-        {
-            name = "ability_custom_spiked_carapace",
-            count = 1
-        },
-        {
-            name = "ability_custom_call_summon_omniknight",
-            count = 1
-        },
-        {
-            name = "ability_custom_call_summon_magnataur",
-            count = 1
-        },
-    }}
-
+    }
+}
    
